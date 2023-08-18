4fd0a86a89cb9b1a89c4b6ecfd69bb15df9f2f83
taeheoki <dkqjwl2365@naver.com>
2023-08-18 21:25:59 +0900

    🧑🏻‍💻feat: 구체 클래스 기반 프록시 - 예제2

--- src/test/java/hello/proxy/pureproxy/concrete/ConcreteProxyTest.java
+++ src/test/java/hello/proxy/pureproxy/concrete/ConcreteProxyTest.java
@@ -2,6 +2,7 @@ package hello.proxy.pureproxy.concrete;
 
 import hello.proxy.pureproxy.concrete.code.ConcreteClient;
 import hello.proxy.pureproxy.concrete.code.ConcreteLogic;
+import hello.proxy.pureproxy.concrete.code.TimeProxy;
 import org.junit.jupiter.api.Test;
 
 public class ConcreteProxyTest {
@@ -10,6 +11,14 @@ public class ConcreteProxyTest {
     public void noProxy() throws Exception {
         ConcreteLogic concreteLogic = new ConcreteLogic();
         ConcreteClient client = new ConcreteClient(concreteLogic);
+        client.execute();
+    }
+
+    @Test
+    public void addProxy() throws Exception {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient client = new ConcreteClient(timeProxy);
         client.execute();
     }
 }
