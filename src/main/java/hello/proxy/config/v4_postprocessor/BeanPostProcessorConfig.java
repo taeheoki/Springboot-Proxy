883a5a590e329cbf302b5ac18522e880d7e400d1
taeheoki <dkqjwl2365@naver.com>
2023-08-30 01:39:57 +0900

    🧑🏻‍💻feat: 빈 후처리기 - 적용

---
+++ src/main/java/hello/proxy/config/v4_postprocessor/BeanPostProcessorConfig.java
@@ -0,0 +1,32 @@
+package hello.proxy.config.v4_postprocessor;
+
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
+import hello.proxy.config.v4_postprocessor.postprocessor.PackageLogTracePostPrecessor;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({AppV1Config.class, AppV2Config.class})
+public class BeanPostProcessorConfig {
+
+    @Bean
+    public PackageLogTracePostPrecessor logTracePostPrecessor(LogTrace logTrace) {
+        return new PackageLogTracePostPrecessor("hello.proxy.app", getAdvisor(logTrace));
+    }
+
+    private Advisor getAdvisor(LogTrace logTrace) {
+        // pointcut
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedNames("request*", "order*", "save*");
+        // advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+}
